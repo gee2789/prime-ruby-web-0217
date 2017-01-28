4ccf44bb0bf1a66e151a1158b5932e43775c979a
Gee Li <geeyung.li@gmail.com>
2017-01-28 02:26:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  if number<0 || number==1 || number==0
+    false
+  else
+  dividers_array=(2...number).to_a
+  remainder_array=[]
+    dividers_array.each do |divider|
+      remainder_array << number%divider
+    end
+  if remainder_array.include? (0)
+    false
+  else
+    true
+  end
+end
+end
+
+    
